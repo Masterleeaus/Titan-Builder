9ba1535dcd7320cec8c86d3a5d3ec6840073fdc0
IOSONOSOI <masterlee.aus@gmail.com>
2026-08-02 14:47:27 +1000

    feat: expose stable tool library exports

---
+++ src/tools/index.ts
@@ -0,0 +1,35 @@
+export {
+  TOOL_APPROVAL_MODES,
+  TOOL_MANIFEST_SCHEMA_VERSION,
+  TOOL_PERMISSIONS,
+  TOOL_PLATFORMS,
+  TOOL_RISKS,
+  TOOL_STATUSES,
+  parseToolManifest,
+  validateToolManifest,
+} from './manifest.js';
+export type {
+  TitanToolManifest,
+  ToolManifestIssue,
+  ToolManifestValidationResult,
+  ToolRisk,
+} from './manifest.js';
+export {
+  builtinToolRegistry,
+  createToolRegistry,
+  isToolId,
+  requiresExplicitApproval,
+} from './catalog.js';
+export type { ToolId } from './catalog.js';
+export type {
+  BuiltinToolDefinition,
+  ToolInputFile,
+  ToolInvocation,
+  ToolRegistry,
+} from './types.js';
+export {
+  isUnsafeLegacyCommandEnabled,
+  listToolManifests,
+  resolveToolInvocation,
+  toolInputFiles,
+} from './registry.js';
